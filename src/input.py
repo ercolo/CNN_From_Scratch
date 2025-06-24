ed78bab72d66bcdcca1560f1feadaad843ca485f
ercolo <victorcolomeet25@gmail.com>
2025-06-24 14:24:37 +0200

    Agggiunte funzioni softmax e loss function e sistemato variabili target

--- src/input.py
+++ src/input.py
@@ -7,17 +7,21 @@ import os
 #Ottenere numero elementi in img
 current_fd = f"CNN_From_Scratch\\img\\tiny-imagenet-200\\train"
 dir_list = os.listdir(current_fd)
-    
+yTrain = []
+
 immagini = [] 
 
 immagini = []
-for i in dir_list:
+for class_ind,i in enumerate(dir_list):
     path_img = os.path.join(current_fd, i, "images")
     files = os.listdir(path_img)
     for f in files:
         immagini.append(os.path.join(path_img, f))
+        yTrain.append(class_ind)
 print("ottenimento path immagini finito")
-    
+
+yTrain = torch.tensor(yTrain).to("cuda")
+
 elementi_dir = 4   #500*200
 
 #Preparazione TENSOR
@@ -87,22 +91,24 @@ def convolution(batch,elementi_dir,bias,batch_conv_out,h,w):
             batch_conv_out[i+1][j] = kernel_conv_img[i+1][j]+bias[j]
             batch_conv_out[i+2][j] = kernel_conv_img[i+2][j]+bias[j]
             print("immagine:",i," kernel_conv:",j)
-def normalizzation(batch_conv_out):
-    for i in range(batch_conv_out.shape[0]):
-        for j in range(batch_conv_out.shape[1]):
-            img_np = batch_conv_out[i][j].cpu().numpy()
+            
+#applicazione della normalizzazione     
+def normalizzation(batch):
+    for i in range(batch.shape[0]):
+        for j in range(batch.shape[1]):
+            img_np = batch[i][j].cpu().numpy()
             img_np = (img_np - img_np.min()) / (img_np.max() - img_np.min())
-            batch_conv_out[i][j] = torch.tensor(img_np, device=batch_conv_out.device)
-    return batch_conv_out
+            batch[i][j] = torch.tensor(img_np, device=batch.device)
+    return batch
+
 
 #applicazione di ReLU dove se x > 0 tiene x altrimenti 0.0    
- 
 def ReLU(batch_conv_out):
     batch_conv_out = torch.where(batch_conv_out>0,batch_conv_out,torch.tensor(0.0))
     return batch_conv_out
 
-#Max pooling della batch in un tensor di Batch,kernel,H/2,W/2
 
+#Max pooling della batch in un tensor di Batch,kernel,H/2,W/2
 def maxPooling(batch_pool_out,batch_conv_out):
     kernel_pool_img = torch.zeros(elementi_dir, 16, int(batch_pool_out.shape[2]//2), int(batch_pool_out.shape[3]//2)).to("cuda")
     for i in range(batch_pool_out.shape[0]):
@@ -112,8 +118,8 @@ def maxPooling(batch_pool_out,batch_conv_out):
                     kernel_pool_img[i][j][k][l] = max(batch_conv_out[i][j][k*2][l*2],batch_conv_out[i][j][k*2+1][l*2],batch_conv_out[i][j][k*2][l*2+1],batch_conv_out[i][j][k*2+1][l*2+1])
     return kernel_pool_img
 
-#Flatteri della batch contenente il max poolin in un tensor da Batch,Kernel*H*W
 
+#Flatteri della batch contenente il max poolin in un tensor da Batch,Kernel*H*W
 def flatter(batch_pool_out,batch_flattern_out,h,w):
     for i in range(0,batch_pool_out.shape[0]):
         for j in range(0,batch_pool_out.shape[1]):
@@ -122,11 +128,27 @@ def flatter(batch_pool_out,batch_flattern_out,h,w):
                     batch_flattern_out[i][(j*(h*w))+(k*w)+(l)] = batch_pool_out[i][j][k][l]
     return batch_flattern_out
 
+def softMax(y,p):
+    for i in range(0,y.shape[0]):
+        row_max = torch.max(y[i])
+        exp_row = torch.exp(y[i] - row_max).to("cuda")
+        sum_exp = torch.sum(exp_row).to("cuda")
+        for j in range(0,y.shape[1]):
+            p[i][j] = exp_row[j] / sum_exp
+    return p
+            
+def lostFunction(yTrain,p,l,lT):
+    for i in range(yTrain.shape[0]):
+        l[i] = -torch.log(p[i][yTrain[i]] + 1e-8)
+    lT = torch.sum(l)/yTrain.shape[0]
+    return l,lT
+
 #Prendere elementi e inseririli nel tensor batch
 for i in range(0,elementi_dir):
     img = cv.imread(immagini[i])
     dataSetInput(batch,img,i)
 print("ottenimento immagini finito")
+
 
 #Convoluzione
 convolution(batch,elementi_dir,bias,batch_conv_out,h,w)
@@ -168,7 +190,10 @@ weights2 = torch.randn(200,200)*0.01                #lunghezza immagini su una s
 weights2 = weights2.to("cuda") 
 bias_FC2 = torch.randn(200)*0.01                    #classi possibili 200/neuroni
 bias_FC2 = bias_FC2.to("cuda") 
-y2 = torch.zeros(elementi_dir,200)  .to("cuda")                 #quantita immagini e neuroni/classi possibili
+y2 = torch.zeros(elementi_dir,200).to("cuda")       #quantita immagini e neuroni/classi possibili
+p = torch.zeros_like(y2)
+l = torch.zeros(elementi_dir).to("cuda")            #tensore contenente ogni loss individuale
+lT = 0
 
 p = 0.8                                             #Probabilità che nel drop out si 0 20%
 yDropOut = (torch.rand_like(y) < p).float().to("cuda") 
@@ -177,7 +202,7 @@ yDropOut = (torch.rand_like(y) < p).float().to("cuda")
 def fullyConnectedLayer(X,weights,bias_FC,y):       #Funzione FC layer
     for i in range(X.shape[0]):
         for j in range(bias_FC.shape[0]):
-            somma = 0
+            somma = (0).to("cuda")
             for k in range(X.shape[1]):
                 somma += X[i][k]*weights[k][j]
             y[i][j] = somma+bias_FC[j]
@@ -189,7 +214,11 @@ y = ReLU(y)             #implementazione ReLU
 
 y = y*yDropOut/p        #implementazione Dropout_mask
 
+y = normalizzation(y)
+
 y2 = fullyConnectedLayer(y,weights2,bias_FC2,y2)    #implementazione FC layer 2
 
+p = softMax(y2,p)
 
+l,lT = lostFunction(yTrain,p,l,lT)
 
